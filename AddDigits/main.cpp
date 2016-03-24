e78bed12e64dc4f29d130c98790b6fd0914a258a
Cloudox <cloudox@foxmail.com>
2016-03-24 14:11:44 +0800

    Initial Commit

---
+++ AddDigits/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AddDigits
+//
+//  Created by csdc-iMac on 16/3/24.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
