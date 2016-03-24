994d4cd0c9ffd2a5d8344829a75e6ccfc6b8cecc
Cloudox <cloudox@foxmail.com>
2016-03-24 14:43:03 +0800

    完成两种方法

--- AddDigits/main.cpp
+++ AddDigits/main.cpp
@@ -9,7 +9,31 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int addDigits2(int num);
+
+    int result = addDigits2(456);
+    printf("result=%d\n",result);
+    
     return 0;
+}
+
+// 方法一：常规的循环
+int addDigits1(int num) {
+    int tempResult = num;// 存储临时加入的结果
+    while (tempResult > 10) {
+        int tempNum = tempResult;
+        tempResult = 0;
+        
+        for (; tempNum / 10 >= 1;) {
+            tempResult += tempNum % 10;
+            tempNum = tempNum / 10;
+        }
+        tempResult += tempNum;
+    }
+    return tempResult;
+}
+
+// 方法二：找规律
+int addDigits2(int num) {
+    return num % 9 == 0 ? (num == 0 ? 0 : 9) : num % 9;
 }
